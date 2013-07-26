473a9798becd217d6f9f30e48b0358076e8d1b91
Matteson <drew.matteson@gmail.com>
2013-07-25 23:29:08 -0400

    Lots of changes, it's starting to become some approximation of useful.  Still needs significant refactoring of the logic to be general.

---
+++ populateCorpus.py
@@ -0,0 +1,20 @@
+from classes import TwitterLogFromQuery
+from classes import TwitterLogFromStream
+from classes import Authorize
+import time
+
+auth = Authorize('authStore.db')
+auth_dict = auth.getData('my_app')
+
+currStartTime = time.time()
+timeout = 300
+stream = TwitterLogFromStream(False,'corpusTweets.db',
+                              float('inf'), 
+                    auth_dict['APP_KEY'],
+                    auth_dict['APP_SECRET'],
+                    auth_dict['OAUTH_TOKEN'],
+                    auth_dict['OAUTH_TOKEN_SECRET'])
+stream.statuses.sample(language='en')
+
+
+
